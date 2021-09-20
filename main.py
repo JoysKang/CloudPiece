cd431097c60024978ab4d7648ab55575ed6766d8
joys <joys.karen@foxmail.com>
2021-09-20 16:37:37 +0800

    更新/start 命令提示

--- main.py
+++ main.py
@@ -57,9 +57,12 @@ async def start(message: Message):
     CloudPiece 能够快速记录你的想法到 Notion 笔记中
     1. 请拷贝 [模板](https://joys.notion.site/fa90a1d7e8404e1286f66941dafd4155) 到自己的 Notion 中 
     2. 使用 /bind 命令授权 CloudPiece 访问，在授权页面选择你刚刚拷贝的模板(注：错误的选择将无法正常使用 CloudPiece)
-    3. 输入 test ，你将收到 【已存储】的反馈，这时你的想法已经写入到 Notion，快去 Notion 看看吧~
+    3. 输入 test,你将收到 【已存储】的反馈，这时你的想法已经写入到 Notion，快去 Notion 看看吧~
     
-    如果 CloudPiece 不能使你满意，你可以到 [反馈页面](https://joys.notion.site/CloudPiece-feedback-3cb2307641184267a6ad7f4f1e97d5a9) 进行反馈, 
+    更详细的教程请看 [使用说明](https://joys.notion.site/CloudPiece-d9c509b20b0d40d4a1076c991416fd2e)
+    
+    如果 CloudPiece 不能使你满意，你可以到 [github](https://github.com/JoysKang/CloudPiece/issues) 提 issues,
+    或发邮件至 licoricepiece@gmail.com, 
     或者直接使用 /unbind 进行解绑，然后到拷贝的模板页，点击右上角的 Share 按钮，从里边移除 CloudPiece
     """
     chat_id = str(message.chat.id)
