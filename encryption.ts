366c79cff414e8cee39d9c56ce76a3e05e8f0010
Xiaokang Fan <xiaokang.fan@eigen.capital>
2022-04-21 16:52:48 +0800

    加密解密

---
+++ encryption.ts
@@ -0,0 +1,12 @@
+const CryptoJS = require("crypto-js");
+
+const Config = require("./conf.json")
+
+
+export function encrypt(text: string) {
+    return CryptoJS.AES.encrypt(text, Config.key).toString();
+}
+
+export function decrypt(text: string) {
+    return CryptoJS.AES.decrypt(text, Config.key).toString(CryptoJS.enc.Utf8);
+}
